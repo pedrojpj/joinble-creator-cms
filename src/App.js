a450ce745f97c026a0526cac7478a968f9f7136a
pedrojose.pena <pedrojose.pena@i3television.es>
2018-02-26 15:37:23 +0100

    (Configuration) Add Relay

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { graphql, QueryRenderer } from 'react-relay';
+import environment from './environment';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query AppQuery {
+            translations
+          }
+        `}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return <div>{props.translations}</div>;
+        }}
+      />
     );
   }
 }

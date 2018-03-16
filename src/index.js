2cac252463ef60e19f3ed885e3e9d618f9528f4e
pedrojose.pena <pedrojose.pena@i3television.es>
2018-03-16 11:17:21 +0100

    perf(React-Perf-Devtool): Add tools for monitoring performance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { registerObserver } from 'react-perf-devtool';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './theme/theme.css';
@@ -8,5 +9,13 @@ import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV === 'development') {
+  const options = {
+    shouldLog: true,
+    port: 3000
+  };
+
+  registerObserver(options);
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

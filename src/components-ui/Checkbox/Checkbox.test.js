9763517119273d497a6a9ba0529d8915f931b1c5
pedro-outvio <pedro@outvio.com>
2018-04-29 00:07:46 +0200

    test(Checkbox): Bugfix test checkbox

--- src/components-ui/Checkbox/Checkbox.test.js
+++ src/components-ui/Checkbox/Checkbox.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import CheckBoxComponent, { Checkbox } from './CheckBox';
+import Checkbox from './CheckBox';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Checkbox Component UI', () => {
   it('render without crashing', () => {
-    shallow(<CheckBoxComponent />);
+    shallow(<Checkbox />);
   });
 });
